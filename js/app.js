6ebeeb4c2a06dbb4b5d35beec399066c3113c6ec
Taylor Thomas K <taylorthomask@johndeere.com>
2022-02-04 12:33:58 -0600

    variablest

--- js/app.js
+++ js/app.js
@@ -1,10 +1,20 @@
 console.log("I am here")
 /*-------------------------------- Constants --------------------------------*/
-
+/
 
 
 /*---------------------------- Variables (state) ----------------------------*/
+//// 1) Define the required variables used to track the state of the game:
+  // None of these variables will need to hold a value when they are defined
 
+	// 1.1) Use an array to represent the squares on the board.    
+let squares = [0, 1,2, 3, 4, 5, 6, 7, 8]
+	// 1.2) Use a turn variable to track whose turn it is.
+
+	// 1.3) Use a winner variable to represent three different game states:
+	  // a player that won
+	  // a tie has occured
+	  // or a game that is still in play.
 
 
 /*------------------------ Cached Element References ------------------------*/
